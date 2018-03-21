c6b251788c17a9a2f0153b311d59a8680a58791e
LizaKarahodina <36096147+LizaKarahodina@users.noreply.github.com>
2018-03-22 01:20:44 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp         = require('gulp'),
+    postcss      = require('gulp-postcss'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    browser      = require('browser-sync'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    consolidate  = require('gulp-consolidate');
+
+
+gulp.task('sass', function () {
+  return gulp.src('sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css'))
+        .pipe(browser.stream({match: '**/*.css'}));
+});
+
+// Builds the documentation and framework files
+//gulp.task('build', ['clean', 'sass', 'javascript']);
+
+// Starts a BrowerSync instance
+gulp.task('serve', ['sass'], function(){
+  browser.init({
+	  	server: {
+            baseDir: "./"
+        }
+  	});
+});
+
+// Runs all of the above tasks and then waits for files to change
+gulp.task('default', ['serve'], function() {    
+  gulp.watch(['sass/**/*.scss'], ['sass']);  
+  gulp.watch('./**/*.html').on('change', browser.reload);
+});
+
+
